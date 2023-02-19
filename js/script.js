e5c042536eafe31fb868526fb62bc186585b391e
Rasheed124 <rashdevatrealglobe.com>
2023-02-19 19:56:03 +0100

    carousel  Updated

--- js/script.js
+++ js/script.js
@@ -1,42 +1,97 @@
-// const colors = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","A", "B", "C", "D", "E", "F"];
-const colors = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
 
-// rgb(0, 0, 0)
+  // select items
+  const img = document.getElementById("person-img");
+  const author = document.getElementById("author");
+  const job = document.getElementById("job");
+  const info = document.getElementById("info");
+  
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const randomBtn = document.querySelector(".random-btn");
+  
+  // set starting item
+  let currentItem = 0;
 
-const btn = document.querySelector("#btn")
-const color = document.querySelector(".color")
+  window.addEventListener("DOMContentLoaded", () => {
+    showPerson(currentItem)
+  })
+
+//   SHow person
+const showPerson = (person)=>{
+    let item = reviews[person];
+    img.src = item.img
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
 
 
-btn.addEventListener("click", () => {
+// Show Next Person
 
-    // GENERATE  WITH LOOP & colors Arrays
-
-    let hex = "#"
-    for (let i = 0; i < 6; i++) {
-        hex += colors[generateColors()]
-
+nextBtn.addEventListener("click", () => {
+    currentItem++;
+    if(currentItem > reviews.length -1){
+        currentItem = 0;
     }
-    document.body.style.backgroundColor = hex;
-    color.textContent = hex;
-
-
-
-    // GENERATE  WITHOUT  LOOP & colors Arrays
-
-    // document.body.style.backgroundColor =  `#${generateColors()}`;
-    // color.textContent = `#${generateColors()}`;
-
+    showPerson(currentItem)
 })
 
 
-const generateColors = () => {
+prevBtn.addEventListener("click", () => {
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length -1;
+    }
+    showPerson(currentItem)
+})
 
 
-    //   Generate Color code Hex 1
-    // return Math.random().toString(16).substring('2', '8') ;
-
-    //   Generate Color code Hex 2
-    return Math.floor(Math.random() * colors.length)
-
+const generateNum = () => {
+    return Math.floor(Math.random() * reviews.length)
 }
+
+randomBtn.addEventListener("click", () => {
+    console.log(generateNum());
+
+    showPerson(generateNum())
+})
 
