d7ae58c1a94015513ebe01d1822f7d9c07dddaac
Rasheed124 <rashdevatrealglobe.com>
2023-02-23 12:29:42 +0100

    App updated

---
+++ js/app.js
@@ -0,0 +1,49 @@
+
+const appData = [
+
+    {
+        id: 1,
+        img: "./img/ramadan1.jpg",
+        dua: "",
+        duaTranslation: "",
+        duaTransleration: "",
+    },
+    {
+        id: 2,
+        img: "./img/ramadan2.jpg",
+        dua: "",
+        duaTranslation: "",
+        duaTransleration: "",
+    },
+    {
+        id: 1,
+        img: "./img/ramadan3.jpg",
+        dua: "",
+        duaTranslation: "",
+        duaTransleration: "",
+    },
+    {
+        id: 1,
+        img: "./img/ramadan4.jpg",
+        dua: "",
+        duaTranslation: "",
+        duaTransleration: "",
+    },
+    {
+        id: 1,
+        img: "./img/ramadan5.jpg",
+        dua: "",
+        duaTranslation: "",
+        duaTransleration: "",
+    },
+    {
+        id: 1,
+        img: "./img/ramadan6.jpg",
+        dua: "",
+        duaTranslation: "",
+        duaTransleration: "",
+    },
+]
+
+const container = document.querySelector(".conatiner");
+
