306b7f6ba3fc9b72037425d02c1eb289ba86c0e1
Rasheed124 <rashdevatrealglobe.com>
2023-02-23 12:47:39 +0100

    App updated

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 // months are ZERO index based;
-const futureDate = new Date(tempYear, tempMonth, tempDay + 30, 12, 00, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 29, 12, 00, 0);
 
 // let futureDate = new Date(2020, 1, 22, 18, 30, 0);
 
