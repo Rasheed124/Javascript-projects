1cd2b812da9b9df9fe24b92b85bfa53729c4154f
Rasheed124 <rashdevatrealglobe.com>
2023-02-25 20:12:31 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-const openMenu  = document.querySelector(".open-menu");
-const menuMain  = document.querySelector(".header-menus");
+const openMenu = document.querySelector(".open-menu");
+const menuMain = document.querySelector(".header-menus");
+const menuOverlay = document.querySelector(".menu-overlay");
 // const menu  = document.querySelector(".menu");
-const closeMenu  = document.querySelector(".close-menu");
+const closeMenu = document.querySelector(".close-menu");
 
 openMenu.addEventListener("click", () => {
-   menuMain.classList.toggle("show-menu")
+   menuMain.classList.toggle("show-menu");
+   // menuOverlay.classList.toggle("show-menu");
+
 })
 
 
 closeMenu.addEventListener("click", () => {
-   menuMain.classList.toggle("show-menu")
+   menuMain.classList.toggle("show-menu");
+   // menuOverlay.classList.toggle("show-menu");
 
 })
