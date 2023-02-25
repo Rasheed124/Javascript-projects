ffa6317bdd887df22ebc31e86865a4da91b15daa
Rasheed124 <rashdevatrealglobe.com>
2023-02-25 23:43:22 +0100

    Updated

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 const openMenu = document.querySelector(".open-menu");
 const menuMain = document.querySelector(".header-menus");
 const menuOverlay = document.querySelector(".menu-overlay");
-// const menu  = document.querySelector(".menu");
 const closeMenu = document.querySelector(".close-menu");
 
+const cartItem = document.querySelector(".cart-items");
+const cartIcon = document.querySelector(".cart-icon");
+
+
+// HEADER CLICKS 
 openMenu.addEventListener("click", () => {
    menuMain.classList.toggle("show-menu");
-   // menuOverlay.classList.toggle("show-menu");
-
 })
 
+menuOverlay.addEventListener("click", (e) => {
+   menuMain.classList.toggle("show-menu");
+})
 
 closeMenu.addEventListener("click", () => {
    menuMain.classList.toggle("show-menu");
-   // menuOverlay.classList.toggle("show-menu");
+})
+
+
+// DISPLAY CART ITEMS CONTAINER
+
+cartIcon.addEventListener("click", () => {
+   cartItem.classList.toggle("show-cart-item")
 
 })
