94d73b244908986251b9ab100e9227bf3c62b042
Rasheed124 <rashdevatrealglobe.com>
2023-04-22 18:50:18 +0100

    GroceryBud Updated

--- script.js
+++ script.js
@@ -1,49 +1,251 @@
-const text = [
-    `Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.`,
-    `Next level tbh everyday carry, blog copper mug forage kitsch roof party pickled hammock kale chips tofu. Etsy shoreditch 8-bit microdosing, XOXO viral butcher banh mi humblebrag listicle woke bicycle rights brunch before they sold out ramps. Twee shabby chic taiyaki flannel, enamel pin venmo vape four loko. Hexagon kale chips typewriter kitsch 8-bit organic plaid small batch keffiyeh ethical banh mi narwhal echo park cronut.`,
-    `Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.`,
-    `Cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers kitty power ignore the squirrels, you'll never catch them anyway for what a cat-ass-trophy! or purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table. Pretend you want to go out but then don't bite off human's toes, yet disappear for four days and return home with an expensive injury; bite the vet so catch eat throw up catch eat throw up bad birds. `,
-    `This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Anyhoo, your net-suits will allow you to experience Fry's worm infested bowels as if you were actually wriggling through them.
-  I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Michelle, I don't regret this, but I both rue and lament it. Morbo can't understand his teleprompter because he forgot how you say that letter that's shaped like a man wearing a hat.`,
-    `Airedale hard cheese mozzarella. Pecorino melted cheese port-salut emmental babybel cheese and wine melted cheese manchego. Everyone loves blue castello everyone loves fromage cheese slices airedale cheddar cream cheese. Bavarian bergkase who moved my cheese halloumi port-salut gouda jarlsberg ricotta rubber cheese. Stinking bishop smelly cheese brie.`,
-    `Salvia glossier subway tile, leggings mustache YOLO semiotics chia. Pitchfork tbh af blog church-key meggings vaporware PBR&B master cleanse post-ironic man bun pabst mustache letterpress synth. Snackwave raw denim godard, 3 wolf moon shaman offal kitsch unicorn live-edge selvage schlitz fashion axe vaporware drinking vinegar prism. Shabby chic tacos artisan, chambray chicharrones cardigan leggings typewriter af pop-up williamsburg meditation PBR&B viral. You probably haven't heard of them DIY jean shorts subway tile fashion axe bushwick kitsch tumeric cloud bread vaporware freegan franzen pork belly chicharrones banh mi.`,
-    `Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.`,
-    `Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.`,
-];
+const alert = document.querySelector('.alert')
+const form = document.querySelector('.grocery-form')
+const grocery = document.querySelector('#grocery')
+const submitBtn = document.querySelector('.submit-btn')
+const container = document.querySelector('.grocery-container')
+const list = document.querySelector('.grocery-list')
+const clearBtn = document.querySelector('.clear-btn')
 
-const form = document.querySelector(".lorem-form");
-const amount = document.getElementById("amount");
-const result = document.querySelector(".lorem-text");
+// edit option
 
+let editElement;
+
+let editFlag = false;
+
+let editId = "";
 
 
-form.addEventListener("submit", (e) => {
+
+
+
+// ********* FUNCTIONS **************
+const addItem = (e) => {
     e.preventDefault();
 
-    const value = parseInt(amount.value);
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
 
-    const random = Math.floor(Math.random() * text.length)
+    if (value && !editFlag) {
+        // adding item to the list
+        creatListItem(id, value);
 
+        // display Alert
+        displayAlert('item added to the list', 'success');
 
+        container.classList.add('show-container');
+        // add to local storage
 
-    // empty
-    // -1
-    // > 9
+        addToLocalStorage(id, value);
+        // set back to default
+        setBackToDefault()
 
-    if (isNaN(value) || value <= 0 || value > 9) {
-        result.innerHTML = `<p class= "result">${text[random]}</p>`;
+    } else if (value && editFlag) {
+
+        editElement.innerHTML = value;
+
+        displayAlert("item updated successfully", "success");
+
+        editLocalStorage(editId, value)
+        setBackToDefault();
     }
     else {
-        let tempText = text.slice(0, value);
+        displayAlert("please enter a value", "danger");
+    }
 
-        tempText = tempText.map(item => {
-            return `<p class="result">${item}</p>`
-        })
-            .join("")
-        result.innerHTML = tempText;
+}
 
 
 
+const setUpItems = () => {
+    let items = getLocalStorage();
+
+    if (items.length > 0) {
+
+        items.forEach(item => {
+            creatListItem(item.id, item.value)
+        });
+
+        container.classList.add("show-container")
 
     }
-})
+}
+
+// display alert
+const displayAlert = (text, action) => {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`)
+
+    // remove alert
+    setTimeout(() => {
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`)
+    }, 1000)
+}
+
+// clear items
+const clearItems = () => {
+    let items = document.querySelectorAll(".grocery-item");
+
+
+    if (items > 0) {
+        items.forEach(item => {
+            list.removeChild(item)
+        });
+    }
+    container.classList.remove("show-container");
+    displayAlert("empty list", "danger");
+    setBackToDefault()
+    localStorage.removeItem('list')
+
+}
+
+// edit item
+const editItem = (e) => {
+    const element = e.currentTarget.parentElement.parentElement;
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+
+    // set form value
+    grocery.value = editElement.innerHTML;
+    editFlag = true;
+    editId = element.dataset.id;
+    submitBtn.textContent = "Edit"
+
+}
+
+// remove item
+const deleteItem = (e) => {
+
+    const element = e.currentTarget.parentElement.parentElement;
+
+    const id = element.dataset.id
+
+    list.removeChild(element);
+
+    if (list.children.length === 0) {
+        container.classList.remove("show-container");
+    }
+    displayAlert("item removed", "danger");
+
+    setBackToDefault();
+
+    // removeFromLocalStorage(id)
+
+}
+
+
+// ********* LOCAL STORAGE **************
+const setBackToDefault = () => {
+    grocery.value = "";
+    editFlag = false;
+    editId = "";
+    submitBtn.textContent = "submit"
+
+}
+
+
+// ********* EVENT LISTENERS **************
+
+// submit form
+form.addEventListener("submit", addItem)
+
+clearBtn.addEventListener("click", clearItems)
+
+
+window.addEventListener("DOMContentLoaded", setUpItems)
+
+// ********* LOCAL STORAGE **************
+
+// add
+const addToLocalStorage = (id, value) => {
+    const grocery = { id, value }
+    let items = getLocalStorage();
+    items.push(grocery);
+    localStorage.setItem("list", JSON.stringify(items))
+}
+
+// remove
+const removeFromLocalStorage = (id) => {
+    let items = getLocalStorage();
+
+    items = items.filter((item) => {
+        if (item.id !== id) {
+            return item;
+        }
+    })
+
+    localStorage.setItem("list", JSON.stringify(items))
+}
+
+const editLocalStorage = (id, value) => {
+    let items = getLocalStorage();
+
+    items = items.map(item => {
+        if (item.id == id) {
+            item.value = value;
+        }
+        return item;
+    });
+
+    localStorage.setItem("list", JSON.stringify(items))
+
+}
+
+const getLocalStorage = () => {
+    return localStorage.getItem("list")
+        ? JSON.parse(localStorage.getItem("list"))
+        : [];
+}
+
+
+// LocalStorage Api
+// setItem
+// getItem
+// save as strings
+
+// localStorage.setItem("names", JSON.stringify(["items", "items2"]))
+
+// const names = JSON.parse(localStorage.getItem("names"))
+// console.log(names);
+
+
+// ********* Setup Items **************
+
+const creatListItem = (id, value) => {
+    const element = document.createElement('article');
+    element.classList.add("grocery-item")
+
+    const attr = document.createAttribute('data-id');
+
+    attr.value = id
+
+    element.setAttributeNode(attr);
+
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+        <button type="button" class="edit-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
+        <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
+        <path d="M16 5l3 3"></path>
+        </svg>
+        </button>
+        <button type="button" class="delete-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M4 7l16 0"></path>
+            <path d="M10 11l0 6"></path>
+            <path d="M14 11l0 6"></path>
+            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+            </svg>
+        </button>
+    </div>`;
+
+    const deleteBtn = element.querySelector('.delete-btn')
+    const editBtn = element.querySelector('.edit-btn');
+    deleteBtn.addEventListener("click", deleteItem)
+    editBtn.addEventListener("click", editItem)
+
+    list.appendChild(element)
+}
